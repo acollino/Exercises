c5e2dc3468282cdd1058ca390af960dd94a403fc
Alex Collino <arcollino@gmail.com>
2022-04-20 14:46:16 -0700

    add support for player color selection

--- Unit_12.2_connect_four_oo/connect4.js
+++ Unit_12.2_connect_four_oo/connect4.js
@@ -10,8 +10,8 @@ class Game {
     this.width = width;
     this.height = height;
     this.board = [];
-    this.player1 = new Player("red").setNumber(1);
-    this.player2 = new Player("blue").setNumber(2);
+    this.player1 = new Player("#ff0000").setNumber(1);
+    this.player2 = new Player("#0000ff").setNumber(2);
     this.currPlayer = this.player1;
     this.gameOver = false;
   }
@@ -61,7 +61,7 @@ class Game {
   placeInTable(y, x) {
     const piece = document.createElement("div");
     piece.classList.add("piece");
-    piece.classList.add(`p${this.currPlayer.number}`);
+    piece.style.backgroundColor = this.currPlayer.color;
     piece.style.top = -50 * (y + 2);
 
     const spot = document.getElementById(`${y}-${x}`);
@@ -79,7 +79,7 @@ class Game {
     }
     const x = +evt.target.id;
     const y = this.findSpotForCol(x);
-    if(y === null || isNaN(x)) {
+    if (y === null || isNaN(x)) {
       return;
     }
 
@@ -87,14 +87,15 @@ class Game {
     this.placeInTable(y, x);
 
     if (this.checkForWin()) {
-      return this.endGame(`Player ${this.currPlayer} won!`);
+      return this.endGame(`Player ${this.currPlayer.number} won!`);
     }
 
     if (this.board.every((row) => row.every((cell) => cell))) {
       return this.endGame("Tie!");
     }
 
-    this.currPlayer = this.currPlayer === this.player1 ? this.player2 : this.player1;
+    this.currPlayer =
+      this.currPlayer === this.player1 ? this.player2 : this.player1;
   }
 
   checkForWin() {
@@ -157,7 +158,7 @@ class Game {
   }
 
   resetGame() {
-    this.currPlayer = 1;
+    this.currPlayer = this.player1;
     this.board = [];
     this.makeBoard();
     this.deletePieces();
@@ -165,7 +166,7 @@ class Game {
   }
 }
 
-class Player{
+class Player {
   constructor(color) {
     this.color = color;
   }
@@ -174,8 +175,39 @@ class Player{
     this.number = num;
     return this;
   }
+
+  setColor(stringColor) {
+    this.color = stringColor;
+  }
 }
 
 let game = new Game(7, 6);
+
 const resetButton = document.querySelector("#play-button");
 resetButton.addEventListener("click", game.startGame.bind(game));
+
+const p1ColorInput = document.querySelector("#p1-color");
+const p2ColorInput = document.querySelector("#p2-color");
+const colorSubmit = document.querySelector("#color-submit");
+colorSubmit.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (colorWarningCheck()) {
+    let choice = confirm(
+      `Warning: Changing a player's color mid-game can be confusing.
+                Proceed anyway?`
+    );
+    if (!choice) {
+      return;
+    }
+  }
+  game.player1.setColor(p1ColorInput.value);
+  game.player2.setColor(p2ColorInput.value);
+});
+
+function colorWarningCheck() {
+  let piecePresent = Boolean(document.querySelector(".piece"));
+  let gameInProgress = !game.gameOver;
+  let p1Changed = p1ColorInput.value !== game.player1.color;
+  let p2Changed = p2ColorInput.value !== game.player2.color;
+  return piecePresent && gameInProgress && (p1Changed || p2Changed);
+}
