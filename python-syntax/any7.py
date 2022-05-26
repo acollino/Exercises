1a25139da3b11e7abd4df814eeeed74adc2856a7
Alex Collino <arcollino@gmail.com>
2022-05-26 13:59:29 -0700

    implement any7

--- python-syntax/any7.py
+++ python-syntax/any7.py
@@ -1,6 +1,9 @@
 def any7(nums):
     """Are any of these numbers a 7? (True/False)"""
-
+    for num in nums:
+        if num == 7:
+            return True
+    return False
     # YOUR CODE HERE 
 
 print("should be true", any7([1, 2, 7, 4, 5]))
