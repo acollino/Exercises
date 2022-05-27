e2f0711359e6a8d7a6eb47b53b1377eb7a99ea7c
Alex Collino <arcollino@gmail.com>
2022-05-26 17:21:25 -0700

    implement convert

--- python-syntax/convert.py
+++ python-syntax/convert.py
@@ -1,3 +1,7 @@
+from ast import IsNot
+from tokenize import Number
+
+
 def convert_temp(unit_in, unit_out, temp):
     """Convert farenheit <-> celsius and return results.
 
@@ -16,6 +20,24 @@ def convert_temp(unit_in, unit_out, temp):
     """
 
     # YOUR CODE HERE
+    invalid_values = []
+    if unit_in != "f" and unit_in != "c":
+        invalid_values.append(unit_in)
+    if unit_out != "f" and unit_out != "c":
+        invalid_values.append(unit_out)
+    if type(temp) != float and type(temp) != int:
+        invalid_values.append(temp)
+    if len(invalid_values) > 0:
+        invalid_values_string = ", ".join(
+            str(value) for value in invalid_values)
+        return f"Invalid unit {invalid_values_string}"
+    elif unit_in == unit_out:
+        return temp
+    else:
+        if unit_in == "f":
+            return (temp - 32) * 5 / 9
+        else:
+            return temp * 9 / 5 + 32
 
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
@@ -23,4 +45,3 @@ print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
 print("z", "f", 32, convert_temp("z", "f", 32), "should be Invalid unit z")
 print("c", "z", 32, convert_temp("c", "z", 32), "should be Invalid unit z")
 print("f", "f", 75.5, convert_temp("f", "f", 75.5), "should be 75.5")
-
