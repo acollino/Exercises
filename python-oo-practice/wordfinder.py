50dfe4b12958cd41e1e52493087c1f43b6032b89
Alex Collino <arcollino@gmail.com>
2022-06-08 09:27:03 -0700

    implement wordfinder

--- python-oo-practice/wordfinder.py
+++ python-oo-practice/wordfinder.py
@@ -1,5 +1,28 @@
 """Word Finder: finds random words from a dictionary."""
-
+from random import choice
 
 class WordFinder:
-    ...
+    """
+    Class that creates a list of words from a file and can return random
+    words from that list
+    """
+
+    def __init__(self, filepath):
+        """Create an instance of WordFinder from a filepath str"""
+        self.filepath = filepath
+        self.word_list = WordFinder.generate_word_list(filepath)
+        print(f"{len(self.word_list)} words read")
+
+    @classmethod
+    def generate_word_list(cls, filepath):
+        """
+        generate a list of strings from a filepath, one string per
+        line in the file
+        """
+        with open(filepath) as word_file:
+            words = [line.strip() for line in word_file]
+            return words
+        
+    def random(self):
+        """return a random word from the word list"""
+        return choice(self.word_list)
