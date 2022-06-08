17a2e9f7078cd499bc5ed8bc060fdd89b15de086
Alex Collino <arcollino@gmail.com>
2022-06-08 08:27:06 -0700

    implement serial.py

--- python-oo-practice/serial.py
+++ python-oo-practice/serial.py
@@ -20,3 +20,18 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start):
+        """Create an instance of a SerialGenerator using a start value"""
+        self.start = start
+        self.increment = start - 1
+    
+    def generate(self):
+        """
+        Generate a value, increasing from start or the last value returned
+        """
+        self.increment += 1
+        return self.increment
+
+    def reset(self):
+        """Reset the SerialGenerator to its starting value"""
+        self.increment = self.start - 1
